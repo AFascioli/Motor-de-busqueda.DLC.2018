fba4b54137c04bd9a01992c23c28cf5752d4b7dd
DiegoPonce1995 <poncedp1995@gmail.com>
2018-05-09 12:04:02 -0300

    09/05/2018

--- MotorDLC/src/java/app/Main.java
+++ MotorDLC/src/java/app/Main.java
@@ -18,6 +18,8 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -28,84 +30,38 @@ public class Main {
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
 
         //========================test==============================
-//        File dir = new File("./Documentos a leer");
-//        File[] archivos = dir.listFiles();
-//        System.out.println("Cantidad de documentos: " + archivos.length);
-//
-//
-//        ArchivoToHM arcToHM = new ArchivoToHM(archivos);
-//        Map aux[] = arcToHM.fileToHM();
-//        System.out.println("Tamaño TerminoHM: " + aux[0].size());
-//        System.out.println("Tamaño PosteoHM: " + aux[1].size());
-//        
-//        archivos=null;
+        File dir = new File("/home/dlcusr/Downloads/DocumentosTP1");
+        File[] archivos = dir.listFiles();
+        System.out.println("Cantidad de documentos: " + archivos.length);
+
+
+        ArchivoToHM arcToHM = new ArchivoToHM(archivos);
+        Map aux[] = arcToHM.fileToHM();
+        System.out.println("Tamaño TerminoHM: " + aux[0].size());
+        System.out.println("Tamaño PosteoHM: " + aux[1].size());
         
-//        TablaPosteo tp = new TablaPosteo("//localhost:1527/MotorDLC");
-
-//        try {
-////            tp.deleteTable("VOCABULARIO");
-//            tp.insertarTerminoHM(aux[0]);
-//            
-//            aux[0]=null;;//PARA LIBERAR MEMORIA????, LE MANDE PARA VER SI AYUDA
-//            
-//            tp.insertarPosteoHM(aux[1]);
-//            
-//            aux[1]=null;;//PARA LIBERAR MEMORIA????, LE MANDE PARA VER SI AYUDA
-//
-//        } catch (ClassNotFoundException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (SQLException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-
-        
-    }
-
-    public static ArrayList rankeo() throws ClassNotFoundException, SQLException {
-
-        String consulta = "the and";//Esto deberia ser un parametro, que es la consulta que hace el usuario
+        archivos=null;
         
         TablaPosteo tp = new TablaPosteo("//localhost:1527/MotorDLC");
 
-        Vocabulario voc =new Vocabulario(tp.loadVocabulario());
-        
-        ArrayList terminosConsulta = voc.obtenerTerminosConsulta(consulta);
-        
-        Map hmDocs = new LinkedHashMap();
+        try {
+//            tp.deleteTable("VOCABULARIO");
+            tp.insertarTerminoHM(aux[0]);
+            
+            aux[0]=null;//PARA LIBERAR MEMORIA????, LE MANDE PARA VER SI AYUDA
+            
+            tp.insertarPosteoHM(aux[1]);
+            
+            aux[1]=null;//PARA LIBERAR MEMORIA????, LE MANDE PARA VER SI AYUDA
 
-        for (Object o : terminosConsulta) {
-
-            Termino term = (Termino) o;
-            ArrayList arrayFR = tp.loadRankeo(term); //array filarankeo de cada termino
-            //)===================Test===============================
-            //HM para los documentos
-
-            for (Object object : arrayFR) {
-
-                FilaRankeo fr = (FilaRankeo) object;
-                Documento docAInsertar = new Documento();
-
-                docAInsertar.setNombre(fr.getDocumento());
-                docAInsertar.setPeso(fr.calcularPeso(arrayFR.size(), 500));//500 es la cantidad total de documentos
-                docAInsertar.setTitulo(fr.getTitulo());
-
-                if (hmDocs.containsKey(docAInsertar.getNombre())) {//Si el documento ya esta en el HM, lo saca y le suma el peso que se calculo antes
-
-                    Documento docAux = (Documento) hmDocs.remove(docAInsertar.getNombre());
-
-                    docAInsertar.setPeso(docAux.getPeso() + docAInsertar.getPeso());
-
-                    hmDocs.put(docAInsertar.getNombre(), docAInsertar);
-                } else {
-                    hmDocs.put(docAInsertar.getNombre(), docAInsertar);//agregas el documentos al HM
-                }
-
-            }
-
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         
-        ArrayList<Documento> resultadoConsulta = new ArrayList<>(hmDocs.values());//Mete lo del hash map enun array list para ordenarlo por peso
-        Collections.sort(resultadoConsulta);//Este es el array list de documento ordenado por peso
-        return resultadoConsulta;
     }
+
+    
 }
---
+++ MotorDLC/src/java/app/Renombrar.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+import bd.Documento;
+import bd.FilaRankeo;
+import bd.TablaPosteo;
+import datos.Termino;
+import datos.Vocabulario;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author dlcusr
+ */
+public class Renombrar {
+
+    public Renombrar() {
+    }
+    
+    
+    public List rankeo(Map vocab, String consulta) throws ClassNotFoundException {
+
+        TablaPosteo tp = new TablaPosteo("//localhost:1527/MotorDLC");
+
+        Vocabulario vocabulario = new Vocabulario(vocab);
+
+        ArrayList terminosConsulta = vocabulario.obtenerTerminosConsulta(consulta);
+        
+        if (terminosConsulta.isEmpty()) {
+            List error=null;
+            return error;
+        }
+        Map hmDocs = new LinkedHashMap();
+
+        for (Object o : terminosConsulta) {
+
+            Termino term = (Termino) o;
+            ArrayList arrayFR = tp.loadRankeo(term); //array filarankeo de cada termino
+            //)===================Test===============================
+            //HM para los documentos
+
+            for (Object object : arrayFR) {
+
+                FilaRankeo fr = (FilaRankeo) object;
+                Documento docAInsertar = new Documento();
+
+                docAInsertar.setNombre(fr.getDocumento());
+                docAInsertar.setPeso(fr.calcularPeso(arrayFR.size(), 500));//500 es la cantidad total de documentos
+                docAInsertar.setTitulo(fr.getTitulo());
+
+                if (hmDocs.containsKey(docAInsertar.getNombre())) {//Si el documento ya esta en el HM, lo saca y le suma el peso que se calculo antes
+
+                    Documento docAux = (Documento) hmDocs.remove(docAInsertar.getNombre());
+
+                    docAInsertar.setPeso(docAux.getPeso() + docAInsertar.getPeso());
+
+                    hmDocs.put(docAInsertar.getNombre(), docAInsertar);
+                } else {
+                    hmDocs.put(docAInsertar.getNombre(), docAInsertar);//agregas el documentos al HM
+                }
+
+            }
+
+        }
+        
+        ArrayList<Documento> resultadoConsulta = new ArrayList<>(hmDocs.values());//Mete lo del hash map en un array list para ordenarlo por peso
+        Collections.sort(resultadoConsulta);
+        
+        return resultadoConsulta.subList(0, 9);//El segundo parametro nos indica cuantos documentos vamos a mostrar 
+    }
+}
